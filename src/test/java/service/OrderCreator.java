5c00ba2ec33e17f4d94ec8d0279556fb5f00273c
Some8uddy <Some8uddy@gmail.com>
2020-03-29 23:19:41 -0300

    Feat: Create TestDataReader class in the service package. Create dev.properties file.
    Ref: Refactor the OrderCreator class in order to load data from properties file.

--- src/test/java/service/OrderCreator.java
+++ src/test/java/service/OrderCreator.java
@@ -4,31 +4,31 @@ import model.Order;
 
 public class OrderCreator {
 
-    private static final String HOMEPAGE_URL = "https://cloud.google.com/";
-    private static final String SEARCH_QUERY = "Google Cloud Platform Pricing Calculator";
-    private static final String NUMBER_OF_INSTANCES = "4";
-    private static final String OPERATING_SYSTEM = "Free: Debian, CentOS, CoreOS, Ubuntu, or other User Provided OS";
-    private static final String VM_CLASS = "Regular";
-    private static final String INSTANCE_TYPE = "n1-standard-8";
-    private static final String NUMBER_OF_GPU = "1";
-    private static final String GPU_TYPE = "NVIDIA Tesla V100";
-    private static final String LOCAL_SSD = "2x375 GB";
-    private static final String DATACENTER_LOCATION = "Frankfurt";
-    private static final String COMMITED_USAGE = "1 Year";
-    private static final String EXPECTED_ESTIMATE_HEADER_NAME = "Compute Engine";
+    private static final String HOMEPAGE_URL = "testdata.order.homepage_url";
+    private static final String SEARCH_QUERY = "testdata.order.search_query";
+    private static final String NUMBER_OF_INSTANCES = "testdata.order.number_of_instances";
+    private static final String OPERATING_SYSTEM = "testdata.order.operating_system";
+    private static final String VM_CLASS = "testdata.order.vm_class";
+    private static final String INSTANCE_TYPE = "testdata.order.instance_type";
+    private static final String NUMBER_OF_GPU = "testdata.order.number_of_gpu";
+    private static final String GPU_TYPE = "testdata.order.gpu_type";
+    private static final String LOCAL_SSD = "testdata.order.local_ssd";
+    private static final String DATA_CENTER_LOCATION = "testdata.order.data_center_location";
+    private static final String COMMITTED_USAGE = "testdata.order.committed_usage";
+    private static final String EXPECTED_ESTIMATE_HEADER_NAME = "testdata.order.expected_estimate_header_name";
 
     public static Order withCredentialsFromProperty() {
-        return new Order(HOMEPAGE_URL,
-            SEARCH_QUERY,
-            NUMBER_OF_INSTANCES,
-            OPERATING_SYSTEM,
-            VM_CLASS,
-            INSTANCE_TYPE,
-            NUMBER_OF_GPU,
-            GPU_TYPE,
-            LOCAL_SSD,
-            DATACENTER_LOCATION,
-            COMMITED_USAGE,
-            EXPECTED_ESTIMATE_HEADER_NAME);
+        return new Order(TestDataReader.getTestData(HOMEPAGE_URL),
+            TestDataReader.getTestData(SEARCH_QUERY),
+            TestDataReader.getTestData(NUMBER_OF_INSTANCES),
+            TestDataReader.getTestData(OPERATING_SYSTEM),
+            TestDataReader.getTestData(VM_CLASS),
+            TestDataReader.getTestData(INSTANCE_TYPE),
+            TestDataReader.getTestData(NUMBER_OF_GPU),
+            TestDataReader.getTestData(GPU_TYPE),
+            TestDataReader.getTestData(LOCAL_SSD),
+            TestDataReader.getTestData(DATA_CENTER_LOCATION),
+            TestDataReader.getTestData(COMMITTED_USAGE),
+            TestDataReader.getTestData(EXPECTED_ESTIMATE_HEADER_NAME));
     }
 }
